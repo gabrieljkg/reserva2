e1930f1c08fe8b8bf7689393a24488746e3b2b8a
Gabriel Calid Brito de Almeida <gabrielcalid@gmail.com>
2026-03-29 14:37:48 -0300

    Update server.ts

--- api/server.ts
+++ api/server.ts
@@ -11,7 +11,7 @@ export default async function handler(req: any, res: any) {
 
   try {
     const session = await stripe.checkout.sessions.create({
-   payment_method_types: ['card'],
+  payment_method_types: ['card', 'boleto'],
       line_items: [{
         price_data: {
           currency: 'brl',
