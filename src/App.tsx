1607c34bc33bb456e1213e306924a8c5d49a7b72
Gabriel Calid Brito de Almeida <gabrielcalid@gmail.com>
2026-03-29 17:32:50 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { ListingDetail } from './pages/ListingDetail';
-import { Checkout } from './pages/Checkout';
-import { ListProperty } from './pages/ListProperty';
-import { Profile } from './pages/Profile';
-import { AdminReservations } from './pages/AdminReservations';
-import { Success } from './pages/Success';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ListingDetail from './pages/ListingDetail';
+import Checkout from './pages/Checkout'; 
+import ListProperty from './pages/ListProperty';
+import Profile from './pages/Profile';
 
-import { CartProvider, useCart } from './context/CartContext';
-
-export default function App() {
+function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/destination/:id" element={<ListingDetail />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/anunciar" element={<ListProperty />} />
-          <Route path="/perfil" element={<Profile />} />
-          <Route path="/admin/reservas" element={<AdminReservations />} />
-          <Route path="/sucesso" element={<Success />} />
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listing/:id" element={<ListingDetail />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/list-property" element={<ListProperty />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
